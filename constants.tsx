1444a374be18a64607c98f3b16e867dfdb2366e1
Executivo Rocha <gabrielsrocha1017@gmail.com>
2026-01-24 07:03:29 -0300

    Add files via upload

--- constants.tsx
+++ constants.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { Bitcoin, Flame, Wallet, Layers } from 'lucide-react';
 
 export const ASSETS_CONFIG = [
-  { symbol: 'BDC', name: 'BodeCoin', color: '#10b981', icon: <Layers size={16} /> },
-  { symbol: 'BTC', name: 'Bitcoin', color: '#F7931A', icon: <Bitcoin size={16} /> },
-  { symbol: 'ETH', name: 'Ethereum', color: '#F97316', icon: <Flame size={16} /> },
-  { symbol: 'SOL', name: 'Solana', color: '#14F195', icon: <Wallet size={16} /> },
+  { symbol: 'BDC', name: 'BodeCoin', color: '#10b981', icon: '/favicon (1).png' },
+  { symbol: 'BTC', name: 'Bitcoin', color: '#F7931A', icon: 'https://assets.coingecko.com/coins/images/1/small/bitcoin.png' },
+  { symbol: 'ETH', name: 'Ethereum', color: '#F97316', icon: 'https://assets.coingecko.com/coins/images/279/small/ethereum.png' },
+  { symbol: 'SOL', name: 'Solana', color: '#14F195', icon: 'https://assets.coingecko.com/coins/images/4128/small/solana.png' },
 ];
 
 export const INITIAL_PARAMS = {
